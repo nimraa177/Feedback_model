618c6faf1b25cb629c43963872a192d8fc4c8b8e
Nimra Naseem Awan <nimra66awan@gmail.com>
2024-07-03 08:12:29 -0500

    added CSS on card

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
+var fbNumber = 4;
 const cancelBtn = document.querySelector(".cancel-btn");
 const submitBtn = document.querySelector(".submit-btn");
 const scoreNumber = document.querySelectorAll(".number");
-
+const modal = document.querySelector(".conatiner2");
+modal.style.display = "none";
 function CloseModal(params) {
   const close = document.querySelector(".container");
   console.log(close);
   close.style.display = "none";
 }
 function selectNumber(number) {
+  fbNumber = number;
   // Remove the active class from all buttons
   var buttons = document.querySelectorAll(".number");
   buttons.forEach(function (button) {
@@ -19,7 +22,12 @@ function selectNumber(number) {
   buttons[number - 1].classList.add("active");
 }
 function submitReview() {
-    const modal = document.querySelector
-    
+  const close = document.querySelector(".container");
+  console.log(close);
+  close.style.display = "none";
+  const modal = document.querySelector(".conatiner2");
+  modal.style.display = "block";
+  const x = document.getElementById("feedback");
+  x.innerHTML = "Thanks, you have selected a rating of : " + fbNumber;
 }
 cancelBtn.addEventListener("click", CloseModal);
